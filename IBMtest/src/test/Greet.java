f5f5c8e664343bc5f073575a779e0afcbfc2533a
anish-nayak <nayak.anish98@gmail.com>
2020-03-09 16:48:44 +0530

    dd

---
+++ IBMtest/src/test/Greet.java
@@ -0,0 +1,16 @@
+package test;
+class a
+{
+public static String func()
+{
+	return "Hello World";
+}
+}
+public class Greet {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(a.func());
+	}
+
+}
